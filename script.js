5c5220d9d833666a83ee0311e2bbf5e62735cc9e
Anuj Kumar Parashar <84987221+anujparashar9876@users.noreply.github.com>
2022-06-24 19:01:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// var topic=prompt("Enter the topic");
+// var content=readLine("Enter content");
+// alert(topic);
+function myfunction(){
+var top=prompt("Enter topic");
+// document.getElementById("html")
+// top.innerHTML +="top"
+// alert(top);
+document.write(top);
+}
+function content(){
+    var con=prompt("Enter content");
+    document.write(con);
+}
